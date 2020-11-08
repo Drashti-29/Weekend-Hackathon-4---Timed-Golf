cecdb6e06d3219798eb05d01527d991da0fe6b32
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-11-08 16:34:06 +0530

    x

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,63 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { time: 0, x: 0, y: 0 };
+    this.sid = React.createRef();
   }
-  componentDidMount() {
-    
-  }
+  startTimer = () => {
+    this.setState({ on: true });
 
-  componentWillUnmount() {
-    
-  }
+    this.sid.current = setInterval(
+      () =>
+        this.setState((prevState) => {
+          return {
+            time: prevState.time + 1
+          };
+        }),
+      1000
+    );
 
-
+    document.addEventListener("keydown", this.arrowKeyHandler);
+  };
+  arrowKeyHandler = (event) => {
+    const key = event.keyCode;
+    let curX = this.state.x;
+    let curY = this.state.y;
+    if (key === 39) {
+      curX += 5;
+      this.setState({ x: curX });
+    } else if (key === 37) {
+      curX -= 5;
+      this.setState({ x: curX });
+    } else if (key === 38) {
+      curY -= 5;
+      this.setState({ y: curY });
+    } else if (key === 40) {
+      curY += 5;
+      this.setState({ y: curY });
+    }
+  };
+  componentDidMount() {}
+  componentWillUnmount() {}
 
   render() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.sid.current);
+      document.removeEventListener("keydown", this.arrowKeyHandler); //this
+    }
     return (
- <>
-</>
+      <>
+        <div id="wrapper">
+          <button className="start" onClick={this.startTimer}>
+            Start
+          </button>
+        </div>
+        <h1 className="heading-timer">{this.state.time}</h1>
+        <div
+          className="ball"
+          style={{ left: this.state.x, top: this.state.y }}
+        ></div>
+        <div className="hole"></div>
+      </>
     );
   }
 }
